339cac478cfc68c85c6ea91a6def3c7dac86c8e4
NguyenThanhNghia <147211357+Nghiakuu@users.noreply.github.com>
2025-12-16 11:44:54 +0700

    Update emailConfig.js

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -2,7 +2,10 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // Hoặc sử dụng host/port nếu không dùng gmail
+    // Render hoặc Cloud Host thường chặn port mặc định, nên cần config thủ công
+    host: process.env.EMAIL_HOST || 'smtp.gmail.com',
+    port: parseInt(process.env.EMAIL_PORT) || 587, // Port 587 thường ổn định hơn trên Cloud
+    secure: process.env.EMAIL_PORT == 465, // true nếu dùng port 465
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
